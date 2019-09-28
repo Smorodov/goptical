55132f26257e068afbc2ba0516cec1d728ad162c
Dibyendu Majumdar <mobile@majumdar.org.uk>
2019-09-28 23:28:35 +0100

    WIP version that reads Bill Claff's data format

--- examples/generic/generic_lens.cpp
+++ examples/generic/generic_lens.cpp
@@ -63,13 +63,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <iterator>
+#include <string>
+#include <vector>
+
 using namespace goptical;
 
 class AsphericCurve : public curve::Rotational {
 
 public:
-  AsphericCurve(double r, double k, double A4, double A6, double A8,
-                double A10 = 0.0, double A12 = 0.0, double A14 = 0.0)
+  AsphericCurve(double r, double k, double A4, double A6, double A8, double A10,
+                double A12 = 0.0, double A14 = 0.0)
       : _r(r), _k(k), _A4(A4), _A6(A6), _A8(A8), _A10(A10), _A12(A12),
         _A14(A14) {}
 
@@ -90,7 +94,6 @@ public:
 private:
   double _r;
   double _k;
-  double _A3;
   double _A4;
   double _A6;
   double _A8;
@@ -99,8 +102,396 @@ private:
   double _A14;
 };
 
+class DescriptiveData {
+public:
+  const std::string &get_title() const { return title_; }
+  void set_title(const std::string &title) { title_ = title; }
 
+private:
+  std::string title_;
+};
 
+class Variable {
+public:
+  Variable(const char *name, const char *value) : name_(name), value_(value) {}
+  const std::string name() const { return name_; }
+  const std::string value() const { return value_; }
+
+private:
+  std::string name_;
+  std::string value_;
+};
+
+class AsphericalData {
+
+public:
+  AsphericalData(int surface_number) : surface_number_(surface_number) {}
+  void add_data(double d) { data_.push_back(d); }
+  int data_points() const { return data_.size(); }
+  double data(int i) const {
+    return i >= 0 && i < data_.size() ? data_[i] : 0.0;
+  }
+
+private:
+  int surface_number_;
+  std::vector<double> data_;
+};
+
+enum LensSurfaceType { surface, aperture_stop };
+
+class SurfaceBuilder {
+public:
+  SurfaceBuilder(int id)
+      : id_(id), surface_type_(LensSurfaceType::surface), radius_(0),
+        thickness_(0), ap_radius_(0), refractive_index_(0), abbe_vd_(0),
+        aspherical_data_(nullptr) {}
+  LensSurfaceType get_surface_type() const { return surface_type_; }
+  void set_surface_type(LensSurfaceType surface_type) {
+    surface_type_ = surface_type;
+  }
+  double get_radius() const { return radius_; }
+  void set_radius(double radius) { radius_ = radius; }
+  double get_thickness() const { return thickness_; }
+  void set_thickness(double thickness) { thickness_ = thickness; }
+  double get_ap_radius() const { return ap_radius_; }
+  void set_ap_radius(double ap_radius) { ap_radius_ = ap_radius; }
+  double get_refractive_index() const { return refractive_index_; }
+  void set_refractive_index(double refractive_index) {
+    refractive_index_ = refractive_index;
+  }
+  double get_abbe_vd() const { return abbe_vd_; }
+  void set_abbe_vd(double abbe_vd) { abbe_vd_ = abbe_vd; }
+  const AsphericalData *get_aspherical_data() const { return aspherical_data_; }
+  void set_aspherical_data(const AsphericalData *aspherical_data) {
+    aspherical_data_ = aspherical_data;
+  }
+  int get_id() const { return id_; }
+
+  // Return < 0 on error
+  double add_surface(sys::Lens &lens);
+
+private:
+  int id_;
+  LensSurfaceType surface_type_;
+  double radius_;
+  double thickness_;
+  double ap_radius_;
+  double refractive_index_;
+  double abbe_vd_;
+  const AsphericalData *aspherical_data_;
+};
+
+class LensSystem {
+public:
+  bool parseFile(const std::string &file_name);
+  double add_surfaces(sys::Lens &lens) {
+    double total = 0.0;
+    for (int i = 0; i < surfaces_.size(); i++) {
+      double thickness = surfaces_[i].add_surface(lens);
+      if (thickness < 0.0)
+        return thickness;
+      total += thickness;
+    }
+    printf("Image position is at %f\n", total);
+    return total;
+  }
+
+  const Variable *find_variable(const char *name) const {
+    for (int i = 0; i < variables_.size(); i++) {
+      if (strcmp(name, variables_[i].name().c_str()) == 0) {
+        return &variables_[i];
+      }
+    }
+    return nullptr;
+  }
+
+  double get_image_height() const {
+    return get_variable_or_value("Image Height");
+  }
+
+  double get_variable_or_value(const char *value) const {
+    if (value[0] == 0)
+      return 0.0;
+    if (isalpha(value[0])) {
+      const Variable *var = find_variable(value);
+      if (var != nullptr) {
+        return strtod(var->value().c_str(), NULL);
+      } else {
+        fprintf(stderr, "Variable %s was not found\n", value);
+        return 0.0;
+      }
+    } else {
+      return strtod(value, NULL);
+    }
+  }
+
+  SurfaceBuilder *find_surface(int id) {
+    for (int i = 0; i < surfaces_.size(); i++) {
+      if (surfaces_[i].get_id() == id)
+        return &surfaces_[i];
+    }
+    return nullptr;
+  }
+
+private:
+  DescriptiveData descriptive_data_;
+  std::vector<Variable> variables_;
+  std::vector<SurfaceBuilder> surfaces_;
+  std::vector<AsphericalData> aspherical_data_;
+};
+
+double SurfaceBuilder::add_surface(sys::Lens &lens) {
+  if (surface_type_ == LensSurfaceType::aperture_stop) {
+    lens.add_stop(ap_radius_, thickness_);
+    return thickness_;
+  }
+  if (aspherical_data_ == nullptr) {
+    if (refractive_index_ != 0.0) {
+      if (abbe_vd_ == 0.0) {
+        fprintf(stderr, "Abbe vd not specified for surface %d\n", id_);
+        return -1.0;
+      }
+      lens.add_surface(
+          radius_, ap_radius_, thickness_,
+          ref<material::AbbeVd>::create(refractive_index_, abbe_vd_));
+    } else {
+      lens.add_surface(radius_, ap_radius_, thickness_);
+    }
+    return thickness_;
+  }
+  double k = aspherical_data_->data(1);
+  double a4 = aspherical_data_->data(2);
+  double a6 = aspherical_data_->data(3);
+  double a8 = aspherical_data_->data(4);
+  double a10 = aspherical_data_->data(5);
+  double a12 = aspherical_data_->data(6);
+  double a14 = aspherical_data_->data(7);
+
+  if (refractive_index_ > 0.0) {
+    lens.add_surface(
+        ref<AsphericCurve>::create(radius_, k, a4, a6, a8, a10),
+        ref<shape::Disk>::create(ap_radius_), thickness_,
+        ref<material::AbbeVd>::create(refractive_index_, abbe_vd_));
+  } else {
+    lens.add_surface(ref<AsphericCurve>::create(radius_, k, a4, a6, a8, a10),
+                     ref<shape::Disk>::create(ap_radius_), thickness_);
+  }
+  return thickness_;
+}
+
+// Sizeof buf must be == sizeof input
+static bool parse_delimited(char *input_start, size_t input_size,
+                            std::vector<const char *> &out_tokens, char *buf,
+                            const char *delimiters) noexcept {
+  out_tokens.clear();
+
+  if (input_size == 0) {
+    return true;
+  }
+  const char *input_ptr = input_start;
+  const char *input_end = input_start + input_size;
+  char *wordp = buf;
+
+  while (*input_ptr && input_ptr != input_end) {
+    char *word = wordp;
+    *wordp = 0;
+
+    bool inquote = false;
+    while (*input_ptr && input_ptr != input_end) {
+      if (word == wordp) {
+        // we are at the beginning for a word, so look
+        // for potential quote
+        if (*input_ptr == '"' && !inquote) {
+          // We are in a quoted word
+          inquote = true;
+          input_ptr++;
+          continue;
+        }
+      }
+      if (inquote) {
+        // We are in a quoted word
+        if (*input_ptr == '"') {
+          // Check if it is an escape - i.e.
+          // double quote
+          if (input_ptr + 1 < input_end && *(input_ptr + 1) == '"') {
+            // escape so we add the quote
+            // character
+            *wordp++ = '"';
+            input_ptr += 2;
+            continue;
+          } else {
+            // not escape so the quoted word
+            // ends here
+            inquote = false;
+            *wordp++ = 0;
+            input_ptr++;
+            if (input_ptr < input_end &&
+                (*input_ptr == ',' || *input_ptr == '\t' ||
+                 (delimiters && strchr(delimiters, *input_ptr)))) {
+              // Skip delimiter
+              // following quote
+              input_ptr++;
+            }
+            break;
+          }
+        } else {
+          // still in quoted word
+          *wordp++ = *input_ptr++;
+          continue;
+        }
+      } else {
+        // Not in quoted word
+        if (*input_ptr == ',' || *input_ptr == '\t' ||
+            (delimiters && strchr(delimiters, *input_ptr))) {
+          // word ends due to delimiter
+          *wordp++ = 0;
+          input_ptr++;
+          break;
+        } else if (*input_ptr == '\r' || *input_ptr == '\n') {
+          // skip line feed or CRLF
+          *wordp++ = 0;
+          if (*input_ptr == '\r' && input_ptr + 1 < input_end &&
+              *(input_ptr + 1) == '\n') {
+            input_ptr++;
+          }
+          input_ptr++;
+          break;
+        } else {
+          *wordp++ = *input_ptr++;
+        }
+      }
+    }
+    out_tokens.push_back(word);
+  }
+  return true;
+}
+
+struct SectionMapping {
+  const char *name;
+  int section;
+};
+
+SectionMapping g_SectionMappings[] = {{"[descriptive data]", 1},
+                                      {"[variable distances]", 2},
+                                      {"[lens data]", 3},
+                                      {"[aspherical data]", 4},
+                                      {NULL, 0}};
+
+static int find_section(const char *name) {
+  int section = 0;
+  for (int i = 0; i < sizeof g_SectionMappings / sizeof(SectionMapping); i++) {
+    if (g_SectionMappings[i].name == NULL) {
+      section = g_SectionMappings[i].section;
+      break;
+    } else if (strcmp(g_SectionMappings[i].name, name) == 0) {
+      section = g_SectionMappings[i].section;
+      break;
+    }
+  }
+  return section;
+}
+
+bool LensSystem::parseFile(const std::string &file_name) {
+
+  FILE *fp = fopen(file_name.c_str(), "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Unable to open file %s: %s\n", file_name.c_str(),
+            strerror(errno));
+    return false;
+  }
+
+  char line[256];                  // input line
+  char buf[256];                   // for tokenizing
+  std::vector<const char *> words; // tokenized words
+  int current_section = 0;         // Current section
+
+  while (fgets(line, sizeof line, fp) != NULL) {
+
+    if (!parse_delimited(line, sizeof line, words, buf, "\t\n")) {
+      continue;
+    }
+    if (words.size() == 0) {
+      continue;
+    }
+    if (words[0][0] == '#') {
+      // comment
+      continue;
+    }
+    if (words[0][0] == '[') {
+      // section name
+      current_section = find_section(words[0]);
+      continue;
+    }
+
+    switch (current_section) {
+    case 1:
+      if (words.size() >= 2 && strcmp(words[0], "title") == 0) {
+        descriptive_data_.set_title(words[1]);
+      }
+      break;
+    case 2:
+      if (words.size() == 2) {
+        variables_.push_back(Variable(words[0], words[1]));
+      }
+      break;
+    case 3: {
+      int id = atoi(words[0]);
+      double radius = 0;
+      double thickness = 0;
+      double ap_radius = 0;
+      double refractive_index = 0;
+      double abbe_vd = 0;
+      LensSurfaceType type = LensSurfaceType::surface;
+      if (words.size() >= 2 && strcmp(words[1], "AS") == 0) {
+        type = LensSurfaceType::aperture_stop;
+      } else {
+        radius = get_variable_or_value(words[1]);
+      }
+      if (words.size() >= 3 && strlen(words[2]) > 0) {
+        thickness = get_variable_or_value(words[2]);
+      }
+      if (words.size() >= 4 && strlen(words[3]) > 0) {
+        refractive_index = get_variable_or_value(words[3]);
+      }
+      if (words.size() >= 5 && strlen(words[4]) > 0) {
+        ap_radius = get_variable_or_value(words[4]) / 2.0;
+      }
+      if (words.size() >= 6 && strlen(words[5]) > 0) {
+        abbe_vd = get_variable_or_value(words[5]);
+      }
+      SurfaceBuilder surface_data(id);
+      surface_data.set_abbe_vd(abbe_vd);
+      surface_data.set_ap_radius(ap_radius);
+      surface_data.set_refractive_index(refractive_index);
+      surface_data.set_radius(radius);
+      surface_data.set_thickness(thickness);
+      surface_data.set_surface_type(type);
+      surfaces_.push_back(surface_data);
+    } break;
+    case 4: {
+      int id = atoi(words[0]);
+      AsphericalData aspherical_data(id);
+      for (int i = 1; i < words.size(); i++) {
+        aspherical_data.add_data(strtod(words[i], NULL));
+      }
+      aspherical_data_.push_back(aspherical_data);
+      SurfaceBuilder *surface_builder = find_surface(id);
+      if (surface_builder == nullptr) {
+        fprintf(stderr, "Ignoring aspherical data as no surface numbered %d\n",
+                id);
+      } else {
+        surface_builder->set_aspherical_data(
+            &aspherical_data_[aspherical_data_.size() - 1]);
+      }
+    } break;
+    default:
+      break;
+    }
+  }
+
+  fclose(fp);
+  return true;
+}
 
 int main(int argc, const char *argv[]) {
   //**********************************************************************
@@ -110,9 +501,9 @@ int main(int argc, const char *argv[]) {
     fprintf(stderr, "Please supply a data file\n");
     exit(1);
   }
-  FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-    fprintf(stderr, "Unable to open file %s: %s\n", argv[1], strerror(errno));
+
+  LensSystem lens_data;
+  if (!lens_data.parseFile(argv[1])) {
     exit(1);
   }
 
@@ -120,50 +511,13 @@ int main(int argc, const char *argv[]) {
 
   /* anchor lens */
   sys::Lens lens(math::Vector3(0, 0, 0));
-
-  double image_height = 42.42;
-  double image_pos = 0;
-  char line[128];
-  while (fgets(line, sizeof line, fp) != NULL) {
-    line[sizeof line - 1] = 0;
-    if (line[0] == '#')
-      continue;
-    int stop = 0;
-    double roc = 0, ap_radius = 0, thickness = 0, refractive_index = 0,
-           abbe_vd = 0;
-    sscanf(line, "%d %lf %lf %lf %lf %lf", &stop, &roc, &ap_radius, &thickness,
-           &refractive_index, &abbe_vd);
-    if (stop == 0) {
-      image_height = roc;
-      printf("Image height=%f\n", image_height);
-    } else if (stop == 3) {
-      lens.add_stop(ap_radius, thickness);
-      printf("Added stop thickness=%f aperture_radius=%f\n", thickness,
-             ap_radius);
-    } else if (stop == 2) {
-      //               roc,            ap.radius, thickness,
-      lens.add_surface(roc, ap_radius, thickness);
-      printf("Added surface radius=%f thickness=%f aperture_radius=%f\n", roc,
-             thickness, ap_radius);
-    } else {
-      //               roc,            ap.radius, thickness,
-      lens.add_surface(
-          roc, ap_radius, thickness,
-          ref<material::AbbeVd>::create(refractive_index, abbe_vd));
-      printf("Added surface radius=%f thickness=%f index=%f v no=%f "
-             "aperture_radius=%f\n",
-             roc, thickness, refractive_index, abbe_vd, ap_radius);
-    }
-    image_pos += thickness;
-  }
-  printf("Image position is at %f\n", image_pos);
-  fclose(fp);
-
+  double image_pos = lens_data.add_surfaces(lens);
   sys.add(lens);
   /* anchor end */
   auto &s1 = lens.get_surface(0);
 
-  sys::Image image(math::Vector3(0, 0, image_pos), image_height);
+  sys::Image image(math::Vector3(0, 0, image_pos),
+                   lens_data.get_image_height());
   sys.add(image);
 
   sys.set_entrance_pupil(s1);
@@ -177,8 +531,7 @@ int main(int argc, const char *argv[]) {
   //  sys::SourcePoint source_point(sys::SourceAtInfinity,
   //                                math::Vector3(0, 0, 1));
 
-  sys::SourcePoint source_point(sys::SourceAtInfinity,
-                                math::Vector3(0, 0.3, 1));
+  sys::SourcePoint source_point(sys::SourceAtInfinity, math::Vector3(0, 0, 1));
 
   // add sources to system
   //  sys.add(source_rays);
@@ -311,6 +664,5 @@ int main(int argc, const char *argv[]) {
       /* anchor end */
     }
   }
-
   return 0;
 }
